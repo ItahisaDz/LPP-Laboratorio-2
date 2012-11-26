82c14deb6344320b1db1fcc2a73eb4b5d74ccae4
Itahisa Diaz <maneit49@gmail.com>
2012-11-26 04:04:02 +0000

    proyect

---
+++ bin/ppt.rb
@@ -0,0 +1,22 @@
+# File : ppt.rb
+
+defeat = { :rock=>:scissor, :paper=>:rock, :scissor=>:paper }
+throws = defeat.keys
+    
+player_throws = (ARGV.shift || '').to_sym
+      
+raise SystaxError, " Jugada incorrecta, debes elegir : '#{opcions.join(', ')}'" unless throws.include? player_throws
+      
+computer_throws = throws.sample
+      
+answer =  case player_throws
+	  when computer_throws
+	    " EMPATE"
+	  when defeat[computer_throws]
+	    " Has perdido -->  #{@computer_throw}  gana a #{@player_throw}"
+	  else 
+	    " Has ganado --> #{@player_throw}  gana a  #{@computer_throw}"
+	  end
+      
+puts answer
+
